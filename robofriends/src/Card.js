e8d4a77c67f593a1a705991cf0701518cdaa0020
chilavertina <urosmartic93@gmail.com>
2023-02-22 20:58:53 +0100

    styling komponenti

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -2,7 +2,7 @@ import React from "react"; //importujemo react kako bi program razumeo jsx
 
 const Card = () => {
   return (
-    <div>
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="robots" src="https://robohash.org/test?200x200" />
       <div>
         <h2>Jane Doe</h2>
