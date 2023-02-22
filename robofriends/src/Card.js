9122c6b725d5832bee495507e9ba7ba568ad8aa4
chilavertina <urosmartic93@gmail.com>
2023-02-22 21:15:21 +0100

    destruktuiranje elemenata

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,13 +1,14 @@
 import React from "react"; //importujemo react kako bi program razumeo jsx
 
 const Card = (props) => {
+  const { name, email, id, username } = props;
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${props.id}?200x200`} />
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.username}</p>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{username}</p>
+        <p>{email}</p>
       </div>
     </div>
   );
